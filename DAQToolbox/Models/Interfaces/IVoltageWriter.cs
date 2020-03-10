2301d602f738fdb810127dca4b37a12774a21e13
Rodney Payne <paynerg2@gmail.com>
2020-03-10 11:45:22 -0400

    Add DOL writer & voltage writer interface.
    These serve as some of the basic building blocks of a DAQ device.

--- DAQToolbox/Models/Interfaces/IVoltageWriter.cs
+++ DAQToolbox/Models/Interfaces/IVoltageWriter.cs
@@ -2,6 +2,8 @@
 {
     public interface IVoltageWriter<T>
     {
+        public string PhysicalChannelName { get; set; }
+        public bool IsInitialized { get; set; }
         void Initialize();
         void Write(T voltage);
     }
