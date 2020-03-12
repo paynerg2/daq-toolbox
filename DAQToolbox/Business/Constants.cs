6d90169244563fa8e6566a426ec20766fa311e9c
Rodney Payne <paynerg2@gmail.com>
2020-03-12 19:44:17 -0400

    Update constants
    Added additional error messages.
    Cleaned usings.

--- DAQToolbox/Business/Constants.cs
+++ DAQToolbox/Business/Constants.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DAQToolbox.Business
+﻿namespace DAQToolbox.Business
 {
     public static class Constants
     {
         public static class ErrorMessages
         {
             public const string INVALID_STREAM = "Invalid stream";
+            public const string INVALID_CHANNEL = "Channel name not recognized";
+            public const string INVALID_WAVEFORM_TYPE = "Selected waveform type is not supported";
         }
     }
 }
