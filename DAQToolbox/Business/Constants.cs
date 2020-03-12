29812e078b3432b138ec45eaea7379360d0f0a4b
Rodney Payne <paynerg2@gmail.com>
2020-03-11 22:04:01 -0400

    Add constants
    Centralized location for error messages and other values that would be distributed to multiple locations in the program.

---
+++ DAQToolbox/Business/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAQToolbox.Business
+{
+    public static class Constants
+    {
+        public static class ErrorMessages
+        {
+            public const string INVALID_STREAM = "Invalid stream";
+        }
+    }
+}
