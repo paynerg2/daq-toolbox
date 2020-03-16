ee4b03ed100bf73e945a611f5100dd110976573a
Rodney Payne <paynerg2@gmail.com>
2020-03-16 19:47:00 -0400

    Add range class
    Class to manage setting symmetrical voltage ranges of the device's allowed values.

---
+++ DAQToolbox/Models/Range.cs
@@ -0,0 +1,41 @@
+﻿using DAQToolbox.Business;
+using System;
+
+namespace DAQToolbox.Models
+{
+    public class SymmetricVoltageRange
+    {
+        /* Describes a symmetric voltage range about 0, in volts (V). */
+
+        private double maximumValue;
+        private double minimumValue;
+        public double MaximumValue 
+        { 
+            get => maximumValue; 
+            set {
+                if (value < 0) throw new ArgumentOutOfRangeException(paramName: "MaximumValue", "Maximum Value cannot be a negative voltage");
+                VerifyIncomingValue(value);
+                maximumValue = value;
+                MinimumValue = -1 * value;
+            } 
+        }
+
+        public double MinimumValue 
+        { 
+            get => minimumValue; 
+            set {
+                if (value > 0) throw new ArgumentOutOfRangeException(paramName: "MinimumValue", "Minimum Value cannot be a positive voltage");
+                VerifyIncomingValue(value);
+                minimumValue = value;
+                maximumValue = -1 * value;
+            } 
+        }
+
+        private void VerifyIncomingValue(double value)
+        {
+            bool valueIsAllowed = Array.Exists(Constants.DeviceProperties.ALLOWED_MAX_VOLTAGES, v => v == Math.Abs(value));
+            if (valueIsAllowed) return;
+            throw new ArgumentException(Constants.ErrorMessages.UNSUPPORTED_VOLTAGE_RANGE);
+        }
+    }
+}
