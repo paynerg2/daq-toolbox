13caa6b013b57055a89a66b7e5e150a03e6f007f
Rodney Payne <paynerg2@gmail.com>
2020-03-16 19:46:15 -0400

    Add buffer class
    Class to manage propeties relating to the device's in-memory storage.

---
+++ DAQToolbox/Business/Buffer.cs
@@ -0,0 +1,15 @@
+﻿namespace DAQToolbox.Business
+{
+    public class Buffer
+    {
+        private readonly int maximumBufferSize = Constants.DeviceProperties.MAXIMUM_BUFFER_SIZE;
+
+        public int CalculateBufferSize(int frequency)
+        {
+            // While f > 109Hz, we have to use the maximum sample rate: 900 kS/s, so we
+            // adjust the number of points in the waveform (worst case: 9 points for 100kHz)
+            return frequency > 109 ? maximumBufferSize :
+                                     frequency / maximumBufferSize;
+        }
+    }
+}
