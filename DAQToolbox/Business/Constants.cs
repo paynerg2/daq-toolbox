d11c4a5b9034218842517d54b678285a3e4ba5da
Rodney Payne <paynerg2@gmail.com>
2020-04-06 09:41:50 -0400

    Add device properties to constants

--- DAQToolbox/Business/Constants.cs
+++ DAQToolbox/Business/Constants.cs
@@ -7,6 +7,13 @@
             public const string INVALID_STREAM = "Invalid stream";
             public const string INVALID_CHANNEL = "Channel name not recognized";
             public const string INVALID_WAVEFORM_TYPE = "Selected waveform type is not supported";
+            public const string UNSUPPORTED_VOLTAGE_RANGE = "Voltage range not supported by the device, please input an accepted range.";
+        }
+
+        public static class DeviceProperties
+        {
+            public const int MAXIMUM_BUFFER_SIZE = 8191;
+            public static double[] ALLOWED_MAX_VOLTAGES = { 10, 5, 1, 0.2 };
         }
     }
 }
